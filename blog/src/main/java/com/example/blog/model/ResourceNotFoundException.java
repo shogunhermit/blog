486f380a26652ae621f3ac9a5a3f4f9ccb9eba20
Muhammed Kone <muhammedkone13@gmail.com>
2026-01-12 21:13:05 +0000

    Revert "Third commit"
    This reverts commit b08164a1bf5a5c1176bc34482cbe0dd1ce67be5c.

--- blog/src/main/java/com/example/blog/model/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.blog.model;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
